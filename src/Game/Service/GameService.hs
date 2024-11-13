258097efd853fe23c0f5572327b9a5c7856c9d56
Matthew <matthewhjt72@gmail.com>
2024-11-13 20:22:15 +0700

    feat: add api response

--- src/Game/Service/GameService.hs
+++ src/Game/Service/GameService.hs
@@ -5,8 +5,15 @@ module Game.Service.GameService (
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import Game.Repository.GameRepository (findGames)
 import Web.Scotty (json, ActionM)
+import Lib (ApiResponse(..))
 
 getAllGames :: ActionM ()
 getAllGames = do
     games <- liftIO findGames
-    json games
+    let response = ApiResponse
+            { code = 200
+            , success = True
+            , message = "Games retrieved successfully."
+            , dataField = games
+            }
+    json response
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,20 @@
-module Lib (runDB) where
+{-# LANGUAGE DeriveGeneric #-}
+module Lib (runDB, ApiResponse(..)) where
 
 import Database.MongoDB ( connect, host, access, master, Action )
+import Data.Aeson (ToJSON)
+import GHC.Generics (Generic)
 
 runDB :: Action IO a -> IO a
 runDB act = do
     pipe <- connect $ host "127.0.0.1"
     access pipe master "local" act
+
+data ApiResponse a = ApiResponse
+    { code    :: Int
+    , success :: Bool
+    , message :: String
+    , dataField :: a
+    } deriving (Show, Generic)
+
+instance ToJSON a => ToJSON (ApiResponse a)
